970382389d30b5e73ea1df72966ac69ca7d2dd88
matheustakasaki <matheus8antunes@gmail.com>
2022-05-10 23:57:27 -0300

    finalizando a função

--- pilha.js
+++ pilha.js
@@ -3,4 +3,12 @@ var elementos = []
 var topo = -1;
 var MAX = 10;
 
-function push(num)
+function push(num){
+
+    if(topo < MAX){
+        topo = topo +1
+    }
+    else{
+        console.log('Pilha está cheia')
+    }
+}
